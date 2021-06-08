3aeb218ae91c2c8ded36f95980f6ada68f759a58
murdoch2439 <murdochkhallz@gmail.com>
2021-06-08 21:22:56 +0100

    entry point of the application

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NavigationWrapper from './src/navigations/navigationContainer';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './src/navigations/toptabNavigation';
+import HomeStackNavigation from './src/navigations/homeStackNavigation';
+
 
 export default function App() {
   return (
+    
+    <NavigationContainer>
+
+    
+    
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light"  backgroundColor="green" />
+      
+      <HomeStackNavigation />
+      
     </View>
+    </NavigationContainer>
+   
+    
   );
 }
 
@@ -15,7 +31,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-});
+})
