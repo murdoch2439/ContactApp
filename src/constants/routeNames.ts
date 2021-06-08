a706596451fbaec98f8a19f21ca8193b419af544
murdoch2439 <murdochkhallz@gmail.com>
2021-06-08 21:20:36 +0100

    This handles all my route names

---
+++ src/constants/routeNames.ts
@@ -0,0 +1,7 @@
+export const DISCUSSION_SCREEN = 'Discussion'
+export const CALLS_SCREEN = 'Call'
+export const CONTACT_SCREEN = 'Contacts'
+export const CONFIGURATION_SCREEN = 'configuration'
+export const HOME_SCREEN = 'Home'
+export const ADD_CONTACT_SCREEN = 'Create new contact'
+
